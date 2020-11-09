360288928485257020b58cd3c56fbf0eeeaebf35
RordyC <rordycrawley@outlook.com>
2020-11-08 19:30:45 -0700

    Created a modular moving platform.

---
+++ Assets/Scripts/Moving_Platform.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moving_Platform : MonoBehaviour
+{
+    [SerializeField]
+    private List<Transform> _positions;
+
+    private Transform _targetPos;
+
+    private int _targetPosIndex = 0;
+
+    [SerializeField]
+    private float _speed = 5f;
+
+    private WaitForSeconds _platformDelay = new WaitForSeconds(1f);
+
+    private void Start()
+    {
+        _targetPos = _positions[0];
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (_targetPos != null)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, _targetPos.position, Time.deltaTime * _speed);
+
+            if (transform.position == _targetPos.position)
+            {
+                _targetPos = null;
+                StartCoroutine(ArrivedAtTargetRoutine());
+            }
+        }
+    }
+
+    IEnumerator ArrivedAtTargetRoutine()
+    {
+        yield return _platformDelay;
+        _targetPosIndex++;
+
+        if (_targetPosIndex == _positions.Count)
+        {
+            _positions.Reverse();
+            _targetPosIndex = 1;
+        }
+
+        _targetPos = _positions[_targetPosIndex];
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+            other.transform.parent = this.transform;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+            other.transform.parent = null;
+    }
+}
